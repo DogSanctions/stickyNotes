b799bf5c48ff668e18bca8751e41a0786778c0d7
Jared Hubbard <hubbard.abstract@gmail.com>
2022-10-08 15:46:42 -0700

    middleware [customer/3rd party]

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
-
+app.use(logger)
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
@@ -17,5 +25,7 @@ app.all('*', (req, res) => {
         res.type('txt'),send('404 Page Not Found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
